6afd430acaf9591017f4a98fb3f95471bcc6c24c
keiya01 <keiya.s.0210@gmail.com>
2019-12-09 15:28:36 +0900

    Updates

---
+++ precache-manifest.226530e37ad41bf8a7ec84214aacc634.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "79b1ce1aba9da8903a0b3dbbfbd28a55",
+    "url": "/portfolio/static/media/thumbnail.79b1ce1a.jpg"
+  },
+  {
+    "revision": "e5c3403814a751f5fd5e",
+    "url": "/portfolio/static/js/runtime~main.e5c34038.js"
+  },
+  {
+    "revision": "d1039ae46d8dce5fb1ea",
+    "url": "/portfolio/static/js/main.d1039ae4.chunk.js"
+  },
+  {
+    "revision": "2ff0348dc34d16102d65",
+    "url": "/portfolio/static/js/3.2ff0348d.chunk.js"
+  },
+  {
+    "revision": "392e9d38801fe269f481",
+    "url": "/portfolio/static/js/2.392e9d38.chunk.js"
+  },
+  {
+    "revision": "7ecc8a99b4573790c282",
+    "url": "/portfolio/static/js/1.7ecc8a99.chunk.js"
+  },
+  {
+    "revision": "d1039ae46d8dce5fb1ea",
+    "url": "/portfolio/static/css/main.14c2fb9a.chunk.css"
+  },
+  {
+    "revision": "99f5497b167344a5d38ab55062b5ae9a",
+    "url": "/portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.63e3bdeadad307fdace286cc9439a2eb.js"
+  "/portfolio/precache-manifest.226530e37ad41bf8a7ec84214aacc634.js"
 );
 
 workbox.clientsClaim();
