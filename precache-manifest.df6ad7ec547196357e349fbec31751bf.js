a208f1e3c9180aceb8fbed05bf4f028e48ad3aba
keiya01 <keiya.s.0210@gmail.com>
2020-03-01 19:30:54 +0900

    Updates

---
+++ precache-manifest.df6ad7ec547196357e349fbec31751bf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "eded4216ffc2134ec031",
+    "url": "/portfolio/static/css/main.6036b538.chunk.css"
+  },
+  {
+    "revision": "eded4216ffc2134ec031",
+    "url": "/portfolio/static/js/main.eded4216.chunk.js"
+  },
+  {
+    "revision": "72818347b5b678cec2ca",
+    "url": "/portfolio/static/js/1.72818347.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "29b51e3cc2ac5ce318e25ce9950ee6bd",
+    "url": "/portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.a3dad9b260dba9ddbeffb7c477faa197.js"
+  "/portfolio/precache-manifest.df6ad7ec547196357e349fbec31751bf.js"
 );
 
 workbox.clientsClaim();
